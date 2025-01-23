8d1e150253f14896584b8d6b7079ff16752c1388
Sumeyye45 <sumeyyeazrak@gmail.com>
2025-01-23 18:57:54 +0300

    Versiyon 1

---
+++ spring-data-jpa/src/main/java/com/SumeyyeAzrak/entites/Address.java
@@ -0,0 +1,25 @@
+package com.SumeyyeAzrak.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(mappedBy = "address")
+    private Customer customer;
+}
